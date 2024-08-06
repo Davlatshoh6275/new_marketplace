b28c268c599cdcbf6f9b848d7c735845681b481c
Davlatshoh6275 <davlatshoh6275@gmail.com>
2024-08-06 12:05:03 +0500

    fix baaner section

--- Components/client/Banner.tsx
+++ Components/client/Banner.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import imgBanner from "@/public/images/banner.png";
+import imgElips from "@/public/images/elips.png";
+import Image from "next/image";
 
 export default function Banner() {
   return (
-    <div>
-      <div>
+    <div className="max-w-[1440px]   my-0 mx-auto xl:w-[1200px]  flex justify-between items-center h-[500px] bg-black mb-16  ">
+      <div className="">
         <h1>Categories</h1>
         <h1>Enhance Your Music Experience</h1>
         <div>
@@ -24,6 +27,22 @@ export default function Banner() {
             <span>Seconds</span>
           </div>
         </div>
+      </div>
+      <div className="flex items-center justify-center w-[60%] h-full relative rounded-full ">
+        <Image
+          src={imgElips}
+          alt="img"
+          width={1}
+          height={1}
+          className="w-[100%] h-[100%]  absolute"
+        />
+        <Image
+          src={imgBanner}
+          alt="img"
+          width={100}
+          height={100}
+          className="object-contain relative w-[90%]"
+        />
       </div>
     </div>
   );
